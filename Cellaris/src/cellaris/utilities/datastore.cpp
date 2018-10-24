7725da9d62bf0be5e9cc905f144630c823efc130
Daniel Ward <39196107+CompBioDan@users.noreply.github.com>
2018-10-24 15:52:38 +0100

    attempt to include flex library

--- Cellaris/src/cellaris/utilities/datastore.cpp
+++ Cellaris/src/cellaris/utilities/datastore.cpp
@@ -83,5 +83,86 @@
 		pos.x = newx; pos.y = newy; pos.z = newz;
 	}
 
+	/**********************************************/
+	myVec3f::myVec3f() {
+		this->pos.x = 0;
+		this->pos.y = 0;
+		this->pos.z = 0;
+	}
+
+	myVec3f::myVec3f(float x, float y, float z) {
+		this->pos.x = x;
+		this->pos.y = y;
+		this->pos.z = z;
+	}
+
+	myVec3f::myVec3f(const myVec3f& v) {
+		this->pos.x = v.pos.x;
+		this->pos.y = v.pos.y;
+		this->pos.z = v.pos.z;
+	}
+
+	myVec3f::~myVec3f() {
+	}
+
+	void myVec3f::normalize() {
+		float d = length();
+		// zero-div may occur.
+		pos.x /= d;
+		pos.y /= d;
+		pos.z /= d;
+
+		if (length() < 0.9)
+			pos.x = 2;
+	}
+
+	float myVec3f::lengthSquared() {
+		return pos.x*pos.x + pos.y*pos.y + pos.z*pos.z;
+	}
+
+	float myVec3f::length() {
+		return sqrt(lengthSquared());
+	}
+
+	void myVec3f::add(const myVec3f& v) {
+		pos.x += v.pos.x;
+		pos.y += v.pos.y;
+		pos.z += v.pos.z;
+	}
+
+	void myVec3f::sub(const myVec3f& v) {
+		pos.x -= v.pos.x;
+		pos.y -= v.pos.y;
+		pos.z -= v.pos.z;
+	}
+
+	void myVec3f::negate(const myVec3f& v) {
+		pos.x = -v.pos.x;
+		pos.y = -v.pos.y;
+		pos.z = -v.pos.z;
+	}
+
+	void myVec3f::scale(float s, const myVec3f& v) {
+		pos.x = s * v.pos.x;
+		pos.y = s * v.pos.y;
+		pos.z = s * v.pos.z;
+	}
+
+	void myVec3f::scale(float s) {
+		pos.x *= s;
+		pos.y *= s;
+		pos.z *= s;
+	}
+
+	void myVec3f::cross(const myVec3f& v1, const myVec3f& v2) {
+		// store on stack once for aliasing-safety
+		// i.e. safe when a.cross(a, b)
+
+		float newx = v1.pos.y*v2.pos.z - v1.pos.z*v2.pos.y;
+		float newy = v1.pos.z*v2.pos.x - v1.pos.x*v2.pos.z;
+		float newz = v1.pos.x*v2.pos.y - v1.pos.y*v2.pos.x;
+		pos.x = newx; pos.y = newy; pos.z = newz;
+	}
+
 //}
 
--- Cellaris/test/cellaris_basicfunctionality.cpp
+++ Cellaris/test/cellaris_basicfunctionality.cpp
@@ -15,11 +15,256 @@
 **/
 
 #include "../lib/stdafx.h"
+#include "../lib/flex/core/NvFlex.h"
 #include "../src/cellaris/cellaris.h"
 #include "../src/cellaris/cell/cell.h"
+//#include "../src/flex/buffers.h"
+
+
+#include "../lib/flex/core/NvFlexExt.h"
+#include "../lib/flex/maths/maths.h"
 #include <ctime>
 
 #include <iostream>
+
+NvFlexSolver* g_flex; // Instance of the NvFlexSolver
+NvFlexLibrary* g_flexLib; // Instance of flex library
+NvFlexParams g_params;
+NvFlexTimers g_timers;
+
+struct SimBuffers // Struct containing all Flex simulation data buffers
+{
+	NvFlexVector<Vec4> positions;
+	NvFlexVector<Vec4> restPositions;
+	NvFlexVector<Vec3> velocities;
+	NvFlexVector<int> phases;
+	NvFlexVector<float> densities;
+	NvFlexVector<Vec4> anisotropy1;
+	NvFlexVector<Vec4> anisotropy2;
+	NvFlexVector<Vec4> anisotropy3;
+	NvFlexVector<Vec4> normals;
+	NvFlexVector<Vec4> smoothPositions;
+	NvFlexVector<Vec4> diffusePositions;
+	NvFlexVector<Vec4> diffuseVelocities;
+	NvFlexVector<int> diffuseIndices;
+	NvFlexVector<int> activeIndices;
+
+	// convexes
+	NvFlexVector<NvFlexCollisionGeometry> shapeGeometry;
+	NvFlexVector<Vec4> shapePositions;
+	NvFlexVector<Quat> shapeRotations;
+	NvFlexVector<Vec4> shapePrevPositions;
+	NvFlexVector<Quat> shapePrevRotations;
+	NvFlexVector<int> shapeFlags;
+
+	// rigids
+	NvFlexVector<int> rigidOffsets;
+	NvFlexVector<int> rigidIndices;
+	NvFlexVector<int> rigidMeshSize;
+	NvFlexVector<float> rigidCoefficients;
+	NvFlexVector<Quat> rigidRotations;
+	NvFlexVector<Vec3> rigidTranslations;
+	NvFlexVector<Vec3> rigidLocalPositions;
+	NvFlexVector<Vec4> rigidLocalNormals;
+
+	// inflatables
+	NvFlexVector<int> inflatableTriOffsets;
+	NvFlexVector<int> inflatableTriCounts;
+	NvFlexVector<float> inflatableVolumes;
+	NvFlexVector<float> inflatableCoefficients;
+	NvFlexVector<float> inflatablePressures;
+
+	// springs
+	NvFlexVector<int> springIndices;
+	NvFlexVector<float> springLengths;
+	NvFlexVector<float> springStiffness;
+
+	NvFlexVector<int> triangles;
+	NvFlexVector<Vec3> triangleNormals;
+	NvFlexVector<Vec3> uvs;
+
+	SimBuffers(NvFlexLibrary* l) :
+		positions(l), restPositions(l), velocities(l), phases(l), densities(l),
+		anisotropy1(l), anisotropy2(l), anisotropy3(l), normals(l), smoothPositions(l),
+		diffusePositions(l), diffuseVelocities(l), diffuseIndices(l), activeIndices(l),
+		shapeGeometry(l), shapePositions(l), shapeRotations(l), shapePrevPositions(l),
+		shapePrevRotations(l), shapeFlags(l), rigidOffsets(l), rigidIndices(l), rigidMeshSize(l),
+		rigidCoefficients(l), rigidRotations(l), rigidTranslations(l),
+		rigidLocalPositions(l), rigidLocalNormals(l), inflatableTriOffsets(l),
+		inflatableTriCounts(l), inflatableVolumes(l), inflatableCoefficients(l),
+		inflatablePressures(l), springIndices(l), springLengths(l),
+		springStiffness(l), triangles(l), triangleNormals(l), uvs(l)
+	{}
+};
+
+SimBuffers* g_buffers;
+
+void MapBuffers(SimBuffers* buffers) // Maps the sim data buffers from the CPU for GPU usage
+{
+	buffers->positions.map();
+	//buffers->restPositions.map();
+	//buffers->velocities.map();
+	//buffers->phases.map();
+	//buffers->densities.map();
+	//buffers->anisotropy1.map();
+	//buffers->anisotropy2.map();
+	//buffers->anisotropy3.map();
+	//buffers->normals.map();
+	//buffers->diffusePositions.map();
+	//buffers->diffuseVelocities.map();
+	//buffers->diffuseIndices.map();
+	//buffers->smoothPositions.map();
+	//buffers->activeIndices.map();
+
+	//// convexes
+	//buffers->shapeGeometry.map();
+	//buffers->shapePositions.map();
+	//buffers->shapeRotations.map();
+	//buffers->shapePrevPositions.map();
+	//buffers->shapePrevRotations.map();
+	//buffers->shapeFlags.map();
+
+	//buffers->rigidOffsets.map();
+	//buffers->rigidIndices.map();
+	//buffers->rigidMeshSize.map();
+	//buffers->rigidCoefficients.map();
+	//buffers->rigidRotations.map();
+	//buffers->rigidTranslations.map();
+	//buffers->rigidLocalPositions.map();
+	//buffers->rigidLocalNormals.map();
+
+	//buffers->springIndices.map();
+	//buffers->springLengths.map();
+	//buffers->springStiffness.map();
+
+	//// inflatables
+	//buffers->inflatableTriOffsets.map();
+	//buffers->inflatableTriCounts.map();
+	//buffers->inflatableVolumes.map();
+	//buffers->inflatableCoefficients.map();
+	//buffers->inflatablePressures.map();
+
+	//buffers->triangles.map();
+	//buffers->triangleNormals.map();
+	//buffers->uvs.map();
+}
+
+void UnmapBuffers(SimBuffers* buffers) // Following CPU mapping/writting to the buffer it then unmaps the buffer to allow asynchronous access to GPU
+{
+	// particles
+	buffers->positions.unmap();
+	buffers->restPositions.unmap();
+	buffers->velocities.unmap();
+	buffers->phases.unmap();
+	buffers->densities.unmap();
+	buffers->anisotropy1.unmap();
+	buffers->anisotropy2.unmap();
+	buffers->anisotropy3.unmap();
+	buffers->normals.unmap();
+	buffers->diffusePositions.unmap();
+	buffers->diffuseVelocities.unmap();
+	buffers->diffuseIndices.unmap();
+	buffers->smoothPositions.unmap();
+	buffers->activeIndices.unmap();
+
+	// convexes
+	buffers->shapeGeometry.unmap();
+	buffers->shapePositions.unmap();
+	buffers->shapeRotations.unmap();
+	buffers->shapePrevPositions.unmap();
+	buffers->shapePrevRotations.unmap();
+	buffers->shapeFlags.unmap();
+
+	// rigids
+	buffers->rigidOffsets.unmap();
+	buffers->rigidIndices.unmap();
+	buffers->rigidMeshSize.unmap();
+	buffers->rigidCoefficients.unmap();
+	buffers->rigidRotations.unmap();
+	buffers->rigidTranslations.unmap();
+	buffers->rigidLocalPositions.unmap();
+	buffers->rigidLocalNormals.unmap();
+
+	// springs
+	buffers->springIndices.unmap();
+	buffers->springLengths.unmap();
+	buffers->springStiffness.unmap();
+
+	// inflatables
+	buffers->inflatableTriOffsets.unmap();
+	buffers->inflatableTriCounts.unmap();
+	buffers->inflatableVolumes.unmap();
+	buffers->inflatableCoefficients.unmap();
+	buffers->inflatablePressures.unmap();
+
+	// triangles
+	buffers->triangles.unmap();
+	buffers->triangleNormals.unmap();
+	buffers->uvs.unmap();
+
+}
+
+SimBuffers* AllocBuffers(NvFlexLibrary* lib)
+{
+	return new SimBuffers(lib);
+}
+
+void DestroyBuffers(SimBuffers* buffers) // Destroy the buffers following completion of the simulation
+{
+	// particles
+	buffers->positions.destroy();
+	buffers->restPositions.destroy();
+	buffers->velocities.destroy();
+	buffers->phases.destroy();
+	buffers->densities.destroy();
+	buffers->anisotropy1.destroy();
+	buffers->anisotropy2.destroy();
+	buffers->anisotropy3.destroy();
+	buffers->normals.destroy();
+	buffers->diffusePositions.destroy();
+	buffers->diffuseVelocities.destroy();
+	buffers->diffuseIndices.destroy();
+	buffers->smoothPositions.destroy();
+	buffers->activeIndices.destroy();
+
+	// convexes
+	buffers->shapeGeometry.destroy();
+	buffers->shapePositions.destroy();
+	buffers->shapeRotations.destroy();
+	buffers->shapePrevPositions.destroy();
+	buffers->shapePrevRotations.destroy();
+	buffers->shapeFlags.destroy();
+
+	// rigids
+	buffers->rigidOffsets.destroy();
+	buffers->rigidIndices.destroy();
+	buffers->rigidMeshSize.destroy();
+	buffers->rigidCoefficients.destroy();
+	buffers->rigidRotations.destroy();
+	buffers->rigidTranslations.destroy();
+	buffers->rigidLocalPositions.destroy();
+	buffers->rigidLocalNormals.destroy();
+
+	// springs
+	buffers->springIndices.destroy();
+	buffers->springLengths.destroy();
+	buffers->springStiffness.destroy();
+
+	// inflatables
+	buffers->inflatableTriOffsets.destroy();
+	buffers->inflatableTriCounts.destroy();
+	buffers->inflatableVolumes.destroy();
+	buffers->inflatableCoefficients.destroy();
+	buffers->inflatablePressures.destroy();
+
+	// triangles
+	buffers->triangles.destroy();
+	buffers->triangleNormals.destroy();
+	buffers->uvs.destroy();
+
+	delete buffers;
+}
+
 
 //using namespace Cellaris;
 
@@ -32,9 +277,9 @@ int main()
 	cellaris sim;
 
 	double simulation_end_time = 52.0, simulation_start_time = 0.0;
-	int number_time_steps = 52000, num_cells = 20, sampling_count = 50;
+	int number_time_steps = 52000, num_cells = 1, sampling_count = 1000;
 	double dt = (simulation_end_time - simulation_start_time) / number_time_steps;
-	myVec3d sim_bounds = myVec3d(2.0f, 2.0f, 2.0f);
+	myVec3f sim_bounds = myVec3f(2.0f, 2.0f, 2.0f);
 	double cell_radius = 0.1;
 
 	// TESTING THE SETUP
@@ -110,6 +355,55 @@ int main()
 	std::cout << "x pos: " << fetched_cell_2->get_cell_position().pos.x << '\n';
 	std::cout << "y pos: " << fetched_cell_2->get_cell_position().pos.y << '\n';
 	std::cout << "z pos: " << fetched_cell_2->get_cell_position().pos.z << '\n';*/
+
+
+	/* Flex Initialisation */
+	//if (g_flex) // If instance of solver already exists remove it
+	//{
+	//	std::cout << "GFLEX EXISTS" << '\n';
+
+	//	if (g_buffers)
+	//		DestroyBuffers(g_buffers);
+
+	//	/*NvFlexDestroySolver(g_flex);
+	//	g_flex = NULL;*/
+	//}
+	// alloc buffers
+	g_buffers = AllocBuffers(g_flexLib);
+
+	// map during initialization
+	MapBuffers(g_buffers);
+
+	/*g_buffers->positions.resize(0);
+	g_buffers->velocities.resize(0);
+	g_buffers->phases.resize(0);
+
+	g_buffers->rigidOffsets.resize(0);
+	g_buffers->rigidIndices.resize(0);
+	g_buffers->rigidMeshSize.resize(0);
+	g_buffers->rigidRotations.resize(0);
+	g_buffers->rigidTranslations.resize(0);
+	g_buffers->rigidCoefficients.resize(0);
+	g_buffers->rigidLocalPositions.resize(0);
+	g_buffers->rigidLocalNormals.resize(0);
+
+	g_buffers->springIndices.resize(0);
+	g_buffers->springLengths.resize(0);
+	g_buffers->springStiffness.resize(0);
+	g_buffers->triangles.resize(0);
+	g_buffers->triangleNormals.resize(0);
+	g_buffers->uvs.resize(0);
+
+	g_buffers->shapeGeometry.resize(0);
+	g_buffers->shapePositions.resize(0);
+	g_buffers->shapeRotations.resize(0);
+	g_buffers->shapePrevPositions.resize(0);
+	g_buffers->shapePrevRotations.resize(0);
+	g_buffers->shapeFlags.resize(0);*/
+
+	// free buffers
+	//DestroyBuffers(g_buffers);
+
 
 	sim.evolve();
 
